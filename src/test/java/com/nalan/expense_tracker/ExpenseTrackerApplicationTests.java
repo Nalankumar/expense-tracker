7163321a243e6beefee82e68bdb145d599b5d3fb
Nalan Kumar <thirunalankumar@gmail.com>
2026-03-01 11:27:47 +0530

    Initialize project

---
+++ src/test/java/com/nalan/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalan.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
